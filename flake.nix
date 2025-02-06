a409d685dc15218aaf19b9c6b4cbdd1ee191b5f0
Michael Aebli <doland@duck.com>
2025-02-06 21:34:17 +0100

    nix flake for docker image creation

--- flake.nix
+++ flake.nix
@@ -1,8 +1,7 @@
 {
-  description = "A dev shell with python312Packages.yfinance, poetry, neovim, and python312Packages.numpy";
+  description = "A dev shell with yfinance, poetry, neovim, numpy and a Docker image build";
 
   inputs = {
-    # Use the unstable channel, which tends to have the latest packages.
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
@@ -12,6 +11,7 @@
       let
         pkgs = import nixpkgs { inherit system; };
       in {
+        # The development shell with the required packages
         devShell = pkgs.mkShell {
           buildInputs = [
             pkgs.python312Packages.yfinance
@@ -20,6 +20,25 @@
             pkgs.python312Packages.numpy
           ];
         };
+
+        # Build a Docker image only on x86_64-linux.
+        # For non-linux systems, you might consider overriding the system.
+        dockerImage = if system == "x86_64-linux" then pkgs.dockerTools.buildImage {
+          name = "my-docker-image";
+          tag = "latest";
+          # The 'contents' attribute specifies what is included in the image.
+          # In this example, we include the same packages as in the devShell.
+          contents = [
+            pkgs.python312Packages.yfinance
+            pkgs.poetry
+            pkgs.neovim
+            pkgs.python312Packages.numpy
+          ];
+          # Configure the image. Here we set the default command to a shell.
+          config = {
+            Cmd = [ "/bin/sh" ];
+          };
+        } else null;
       }
     );
 }
